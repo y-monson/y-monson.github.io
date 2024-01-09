1549a09d918fc3f1f0c22647b44c38886759ebda
Yelena Monson <126672225+y-monson@users.noreply.github.com>
2024-01-09 12:09:30 +0100

    Add files via upload

--- app_3.js
+++ app_3.js
@@ -1,6 +1,8 @@
 
 const cards = document.querySelectorAll('.card')
 
+// Each card is assigned the class="flipped" which hides the 
+// skill name and logo and sisplays the skill level
 cards.forEach(card => {
   card.addEventListener('click', () => {
     card.classList.toggle('flipped')
